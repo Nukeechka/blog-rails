bbd171f93e4acd332f38d2894a0c21df1d010ad3
nukeechka <nukeechka@gmail.com>
2025-06-03 23:55:00 +0500

    rails is awesome

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-  get "/articles", to: "articles#index"
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
@@ -12,4 +11,7 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   root "articles#index"
+  resources :articles do
+    resources :comments
+  end
 end
